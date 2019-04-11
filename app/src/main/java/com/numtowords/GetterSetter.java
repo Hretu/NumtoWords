78d27560da4fa2d9ffb6f94aab6f1f299d50c687
Softwarica College Lab <softwarica@softwarica.com>
2019-04-11 16:05:25 +0545

    Initial commit

---
+++ app/src/main/java/com/numtowords/GetterSetter.java
@@ -0,0 +1,16 @@
+package com.numtowords;
+
+public class GetterSetter {
+
+
+    public String getEnternumbmer() {
+        return enternumbmer;
+    }
+
+    public void setEnternumbmer(String enternumbmer) {
+        this.enternumbmer = enternumbmer;
+    }
+
+    private String enternumbmer;
+
+}
